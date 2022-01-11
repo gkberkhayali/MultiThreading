b1fe421e9e9ad27eb6f24757469cefdee0d18367
Gökberk Hayali <gokberkhayali@gmail.com>
2022-01-11 17:44:46 +0100

    C# multithreaing with lock and Mutex.
    C# Threading
    C# Thread Safety- Lock
    C# Mutex

---
+++ MultiThreadCSharp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+
+namespace MultiThreadCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to multiThreading");
+
+            BaseClass myInstance = new BaseClass();
+
+            //System.OverflowException: 'Arithmetic operation resulted in an overflow.'
+            //Normaly with this setup you would get this error above. Because multiple threads are overwriting eachothers values.
+            //To solve this issues we use lock.
+
+            Thread thread1 = new Thread(new ThreadStart(myInstance.DivideLoop));
+            Thread thread2 = new Thread(new ThreadStart(myInstance.DivideLoop));
+
+            thread1.Start();
+            thread2.Start();
+
+            Console.Read();
+
+        }
+
+        public class BaseClass
+        {
+            int num1 = 0;
+            int num2 = 0;
+            int num3 = 0;
+
+            public void DivideLoop()
+            {
+                for (int i = 0; i < int.MaxValue; i++)
+                {
+                    lock(this)
+                    {
+                        num1 = 1;
+                        num2 = 1;
+                        num3 = 0;
+
+                        num3 = num2 / num1;
+
+                        num2 = 0;
+                        num1 = 0;
+                    }
+                    
+                }
+            }
+
+
+        }
+    }
+}
